eb9cd54350285c51a57240ec105f75ee53b972ab
SubSole <33077379+SubSole@users.noreply.github.com>
2017-10-25 00:12:13 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -37,7 +37,7 @@ function respond() {
   } 
   else if(request.text && botRegexHelp.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("commands: lists available commands");
+    postMessage("commands: lists available commands /nW");
     postMessage("garfield: posts today's garfield strip");
     this.res.end();
   } 
