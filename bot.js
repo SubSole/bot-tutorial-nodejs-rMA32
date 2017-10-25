b25f22c15272cc7c77925b485e31c73e1f0ee6f7
SubSole <33077379+SubSole@users.noreply.github.com>
2017-10-25 00:06:44 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -37,7 +37,7 @@ function respond() {
   } 
   else if(request.text && botRegexHelp.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("garfield: posts today's garfield strip /n commands: lists commands");
+    postMessage("garfield: posts today's garfield strip" + /n + "commands: lists commands");
     this.res.end();
   } 
   else if(request.text && botRegexProp.test(request.text)) {
