929df212c66e0599f7d4fe0267dd1d9f6185791b
SubSole <33077379+SubSole@users.noreply.github.com>
2017-10-25 13:47:46 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,6 @@ function respond() {
   var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
                 ,"BAL","SD","DEN","MIN","ATL","KC","NYG","GB","DET","HOU","STL","CHI","CAR",
                 "MIA","BUF","SF","WAS","NYJ","TB"]
-  var today = Date.now()
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
     postMessage(cool());
