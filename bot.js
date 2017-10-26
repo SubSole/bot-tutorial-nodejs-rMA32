4a6d79cd23759a733b2f960a0a0bebc52c049c8a
SubSole <33077379+SubSole@users.noreply.github.com>
2017-10-26 01:59:16 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,7 +18,7 @@ function respond() {
       botRegexDaf = /^\/dafuq/; botRegexMA32 = /^\/pending/; botRegexTrade = /^\/trade/; 
       botRegexGarf = /^\/garfield/; botRegexHelp = /^\/help/; botRegexCommands = /^\/commands/;
       botRegexTest1 = /^\/test1/; botRegexKillMax = /^\/killmax/; botRegexKillJeff = /^\/killjeff/;
-      botRegex69 = /69/; botRegexKillAdam = /^\/killadam/; botRegexKillLee = /^\/killlee/; 
+      botRegex69 = /69/; botRegexKillAdam = /^\/killadam/; botRegexKillLee = /^\/killlee/; botRegexKnicks = /Knicks/; 
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
   var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
@@ -32,6 +32,11 @@ function respond() {
   else if(request.text && botRegex69.test(request.text)) {
     this.res.writeHead(200);
     postMessage("nice");
+    this.res.end();
+  } 
+  else if(request.text && botRegexKnicks.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("Fuck Jim Dolan");
     this.res.end();
   } 
   else if(request.text && botRegexKillMax.test(request.text)) {
