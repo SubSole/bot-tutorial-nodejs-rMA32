8c67319787542c84f5854bcc9ed131b81654c624
SubSole <33077379+SubSole@users.noreply.github.com>
2017-10-26 17:44:05 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ function respond() {
       botRegexP = /^\/PDL/i;  botRegexTw = /^\/twitch/i; botRegexId = /^\/id/;
       botRegexSiege = /^\/siege/; botRegexKill = /^\/kill/i; botRegexLockhorns = /^\/lockhorns/;
       botRegexGarf = /^\/garfield/; botRegexHelp = /^\/help/; botRegexCommands = /^\/commands/;
-      botRegexTest1 = /^\/test1/; 
+      botRegexTest1 = /^\/test1/; botRegexNYK = /knicks/; 
       botRegex69 = /69/; botRegexKnicks = /Knicks/; 
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
@@ -31,6 +31,11 @@ function respond() {
     this.res.end();
   } 
   else if(request.text && botRegexKnicks.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("Fuck Jim Dolan");
+    this.res.end();
+  }   
+  else if(request.text && botRegexNYK.test(request.text)) {
     this.res.writeHead(200);
     postMessage("Fuck Jim Dolan");
     this.res.end();
