de9358511c297913a9630b03b385ab0af0b2bd89
SubSole <33077379+SubSole@users.noreply.github.com>
2017-10-27 18:36:13 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@ function respond() {
       botRegexSiege = /^\/siege/; botRegexKill = /^\/kill/i; botRegexLockhorns = /^\/lockhorns/;
       botRegexGarf = /^\/garfield/; botRegexHelp = /^\/help/; botRegexCommands = /^\/commands/;
       botRegexTest1 = /^\/test1/; botRegexNYK = /knicks/; botRegexWalt = /walt/; 
-      botRegex69 = /69/; botRegexKnicks = /Knicks/; 
+      botRegex69 = /69/; botRegexKnicks = /Knicks/; botRegexWalt1 = /Walt/; 
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
   var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
@@ -31,6 +31,11 @@ function respond() {
     this.res.end();
   } 
   else if(request.text && botRegexWalt.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("Fuck walt");
+    this.res.end();
+  } 
+  else if(request.text && botRegexWalt1.test(request.text)) {
     this.res.writeHead(200);
     postMessage("Fuck walt");
     this.res.end();
